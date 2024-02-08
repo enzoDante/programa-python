d454696fc22f8b9b09b591d32129737564c2fc1d
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2024-02-07 22:35:45 -0300

    clique

---
+++ aprendendo/Coisas_aleatorias/clique_mouse.py
@@ -0,0 +1,19 @@
+import pyautogui
+import time
+# print('Posicione o MOUSE')
+# time.sleep(3)
+# x, y = pyautogui.position()
+# print ("Posicao atual do mouse:")
+# print ("x = "+str(x)+" y = "+str(y))
+
+#retorna Truee se x & y estiverem dentro da tela
+# print ("\nEsta dentro da tela?")
+# resp = pyautogui.onScreen(x, y)
+# print (str(resp))
+
+pyautogui.click(1105, 1067)
+for i in range(5):
+    pyautogui.moveTo(920, 523, 0)
+    pyautogui.click()
+    time.sleep(0.1)
+pyautogui.hotkey('alt', 'tab')
