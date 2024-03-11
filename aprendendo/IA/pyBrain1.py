21831d47137442b99e59da25254bc4522bac543b
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2024-03-11 17:48:30 -0300

    utilizando pybrain

--- aprendendo/IA/pyBrain1.py
+++ aprendendo/IA/pyBrain1.py
@@ -1,1 +1,33 @@
+import sys, numpy
+sys.modules["scipy.random"] = numpy.random
+from pybrain.structure import FeedForwardNetwork
+from pybrain.structure import LinearLayer, SigmoidLayer, BiasUnit
+from pybrain.structure import FullConnection
+
+rede = FeedForwardNetwork()
+
+camadaEntrada = LinearLayer(2) #total entradas, saidas, ocultas
+camadaOculta = SigmoidLayer(3)
+camadaSaida = SigmoidLayer(1)
+bias1 = BiasUnit()
+bias2 = BiasUnit()
+#adicionando as camadas e bias na rede
+rede.addModule(camadaEntrada)
+rede.addModule(camadaOculta)
+rede.addModule(camadaSaida)
+rede.addModule(bias1)
+rede.addModule(bias2)
+#conexao entre as camadas
+entradaOculta = FullConnection(camadaEntrada, camadaOculta)
+ocultaSaida = FullConnection(camadaOculta, camadaSaida)
+biasOculta = FullConnection(bias1, camadaOculta)
+biasSaida = FullConnection(bias2, camadaSaida)
+
+rede.sortModules()
+
+print(rede)
+print(entradaOculta.params)#pesos aleatórios gerados
+print(ocultaSaida.params)
+print(biasOculta.params)
+print(biasSaida.params)
 
